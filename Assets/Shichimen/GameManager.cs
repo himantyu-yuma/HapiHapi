08dfd15aa9032894e62e893864d2f1b1852bb2a5
秋田拓海 <87998840+AkitakuM@users.noreply.github.com>
2022-01-29 19:43:10 +0900

    ゲーム終了実装
    エスケープキーで終了します

--- Assets/Shichimen/GameManager.cs
+++ Assets/Shichimen/GameManager.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     GameObject player;
     GameObject enemy;
-    
-
-    enum SEs 
-    {
-        GetItem=0,BulletHitPlayer,BulletHitEnemy
-    }
 
     // Start is called before the first frame update
     void Start()
@@ -24,16 +19,18 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Keyboard.current.escapeKey.wasPressedThisFrame) GameExit();
     }
 
     public void SceneChange(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
-
-    public void PlaySE(int SEIndex)
+    public void GameExit()
     {
 
+      UnityEditor.EditorApplication.isPlaying = false;
+      Application.Quit();
     }
+    
 }
