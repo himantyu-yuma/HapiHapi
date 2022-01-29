14c7f900573bf5579731226b1864c1ed3c0e6eca
秋田拓海 <87998840+AkitakuM@users.noreply.github.com>
2022-01-29 20:23:23 +0900

    ゲームオーバー実装
    Yキーを押すと最初に戻る

--- Assets/Shichimen/Scripts/GameManager.cs
+++ Assets/Shichimen/Scripts/GameManager.cs
@@ -6,31 +6,30 @@ using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
-    GameObject player;
-    GameObject enemy;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        this.player = GameObject.Find("");
-        this.enemy = GameObject.Find("");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Keyboard.current.escapeKey.wasPressedThisFrame) GameExit();
+        if (Keyboard.current.yKey.wasPressedThisFrame) GameOver();
     }
 
-    public void SceneChange(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
-    }
     public void GameExit()
     {
-
       UnityEditor.EditorApplication.isPlaying = false;
       Application.Quit();
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     
 }
